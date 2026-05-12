9fa4beada0ee2bd42a189c8f031e0b19171918bf
PaulSakyi <paulsak123@gmail.com>
2026-05-12 17:21:43 +0000

    updated site

--- script.js
+++ script.js
@@ -90,31 +90,3 @@ function searchProducts() {
     });
 
 }
-
-// DARK MODE TOGGLE SWITCH
-
-const toggle = document.getElementById("darkModeToggle");
-const modeText = document.getElementById("modeText");
-
-toggle.addEventListener("change", function(){
-
-    document.body.classList.toggle("dark-mode");
-
-    if(document.body.classList.contains("dark-mode")){
-        modeText.innerText = "Light Mode";
-    } else {
-        modeText.innerText = "Dark Mode";
-    }
-
-});
-
-// MOBILE MENU TOGGLE
-
-const menuToggle = document.getElementById("menuToggle");
-const navLinks = document.getElementById("navLinks");
-
-menuToggle.addEventListener("click", function(){
-
-    navLinks.classList.toggle("active");
-
-});
